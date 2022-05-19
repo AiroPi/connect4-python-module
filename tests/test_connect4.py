37a2383f90fa3933976ece58042798944c84fce9
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2022-05-19 16:59:41 +0200

    feat: start add some tests

---
+++ tests/test_connect4.py
@@ -0,0 +1,44 @@
+from pprint import pprint
+
+import connect4
+import pytest
+
+
+def test_init():
+    c4 = connect4.Connect4(dimensions=(4, 4))
+    assert c4.board == [[0] * 4 for i in range(4)]
+    assert c4.strboard() == ((' ' * 4 + '\n') * 4)[:-1]
+
+
+def test_vertical_victory():
+    c4 = connect4.Connect4()
+    assert c4.play(0) == set()
+    assert c4.play(1) == set()
+    assert c4.play(0) == set()
+    assert c4.play(1) == set()
+    assert c4.play(0) == set()
+    assert c4.play(1) == set()
+    assert c4.play(0) == {(5, 0), (4, 0), (3, 0), (2, 0)}
+
+    with pytest.raises(connect4.GameOver) as exception:
+        c4.play(1)
+        assert exception.value.winner == 1
+
+def test_diagonal1_victory():
+    c4 = connect4.Connect4()
+    c4.play(0)
+    c4.play(1)
+    c4.play(1)
+    c4.play(2)
+    c4.play(2)
+    c4.play(6)
+    c4.play(2)
+    c4.play(3)
+    c4.play(3)
+    c4.play(3)
+    assert c4.play(3) == {(2, 3), (3, 2), (4, 1), (5, 0)}
+
+    with pytest.raises(connect4.GameOver) as exception:
+        c4.play(3)
+        print(exception.value.winner)
+        assert exception.value.winner == 1
