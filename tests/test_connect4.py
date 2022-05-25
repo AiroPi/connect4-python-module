421f1d1673b6dcbfadbe0ce76afafb258b2aac1b
Pierre (Shadow) <47398145+AiroPi@users.noreply.github.com>
2022-05-25 22:53:02 +0200

    feat(tests): add multiple win test

--- tests/test_connect4.py
+++ tests/test_connect4.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+from sqlite3 import connect
 
 import connect4
 import pytest
@@ -10,72 +11,54 @@ def test_init():
     assert c4.strboard() == ((' ' * 4 + '\n') * 4)[:-1]
 
 
-def test_vertical_victory():
+def _test_victories(plays: tuple[int, ...], win_points: set[tuple[int, int]], winner: connect4.Players):
     c4 = connect4.Connect4()
-    assert c4.play(0) == set()
-    assert c4.play(1) == set()
-    assert c4.play(0) == set()
-    assert c4.play(1) == set()
-    assert c4.play(0) == set()
-    assert c4.play(1) == set()
-    assert c4.play(0) == {(5, 0), (4, 0), (3, 0), (2, 0)}
+    for play in plays[:-1]:
+        assert c4.play(play) == set()
+
+    assert c4.play(plays[-1]) == win_points
 
     with pytest.raises(connect4.GameOver) as exception:
-        c4.play(1)
+        c4.play(0)
 
-    assert exception.value.winner == connect4.Players.ONE
+    assert exception.value.winner == winner
+
+
+def test_vertical_victory():
+    _test_victories(
+        (0, 1, 0, 1, 0, 1, 0),
+        {(5, 0), (4, 0), (3, 0), (2, 0)},
+        connect4.Players.ONE
+    )
 
 
 def test_horizontal_victory():
-    c4 = connect4.Connect4()
-    c4.play(0)
-    c4.play(0)
-    c4.play(1)
-    c4.play(1)
-    c4.play(2)
-    c4.play(2)
-    assert c4.play(3) == {(5, 0), (5, 1), (5, 2), (5, 3)}
-
-    with pytest.raises(connect4.GameOver) as exception:
-        c4.play(1)
-
-    assert exception.value.winner == connect4.Players.ONE
+    _test_victories(
+        (0, 0, 1, 1, 2, 2, 3),
+        {(5, 0), (5, 1), (5, 2), (5, 3)},
+        connect4.Players.ONE
+    )
 
 
 def test_diagonal1_victory():
-    c4 = connect4.Connect4()
-    c4.play(0)
-    c4.play(1)
-    c4.play(1)
-    c4.play(2)
-    c4.play(2)
-    c4.play(6)
-    c4.play(2)
-    c4.play(3)
-    c4.play(3)
-    c4.play(3)
-    assert c4.play(3) == {(2, 3), (3, 2), (4, 1), (5, 0)}
-
-    with pytest.raises(connect4.GameOver) as exception:
-        c4.play(3)
-
-    assert exception.value.winner == connect4.Players.ONE
+    _test_victories(
+        (0, 1, 1, 2, 2, 6, 2, 3, 3, 3, 3),
+        {(2, 3), (3, 2), (4, 1), (5, 0)},
+        connect4.Players.ONE
+    )
 
 
 def test_diagonal2_victory():
-    c4 = connect4.Connect4()
-    c4.play(5)
-    c4.play(6)
-    c4.play(4)
-    c4.play(5)
-    c4.play(4)
-    c4.play(4)
-    c4.play(3)
-    c4.play(3)
-    c4.play(3)
-    assert c4.play(3) == {(5, 6), (4, 5), (3, 4), (2, 3)}
+    _test_victories(
+        (5, 6, 4, 5, 4, 4, 3, 3, 3, 3),
+        {(5, 6), (4, 5), (3, 4), (2, 3)},
+        connect4.Players.TWO
+    )
 
-    with pytest.raises(connect4.GameOver) as exception:
-        c4.play(3)
 
-    assert exception.value.winner == connect4.Players.TWO
+def test_multiple_victory():
+    _test_victories(
+        (0, 0, 0, 0, 1, 1, 1, 1, 2, 5, 2, 5, 2, 2, 6, 3, 6, 3, 6, 3, 5, 3),
+        {(2, 0), (2, 1), (2, 2), (2, 3), (5, 3), (4, 3), (3, 3), (2, 3)},
+        connect4.Players.TWO
+    )
