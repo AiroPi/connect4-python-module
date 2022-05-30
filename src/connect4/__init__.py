a8910a57d1f4a51fbd831ea6d4f22a094b190afe
Pierre (Shadow) <47398145+AiroPi@users.noreply.github.com>
2022-05-30 20:27:50 +0200

    feat: add linter workflow.

--- src/connect4/__init__.py
+++ src/connect4/__init__.py
@@ -1,2 +1,12 @@
-from .core import *
-from .errors import *
+__title__ = 'connect4.py'
+__author__ = 'airo.pi_'
+__license__ = 'MIT'
+__version__ = '1.0.1'
+
+from .core import Connect4 as Connect4
+from .core import Players as Players
+from .errors import ColumnFull as ColumnFull
+from .errors import GameOver as GameOver
+from .errors import InvalidColumn as InvalidColumn
+
+__all__ = ['Connect4', 'Players', 'ColumnFull', 'GameOver', 'InvalidColumn']
